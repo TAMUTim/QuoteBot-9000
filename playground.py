a0465061d691a48710f6d86cca92700fe028616c
TAMUTim <chrometor@tamu.edu>
2023-08-09 16:14:08 -0500

    Query OpenAI

--- playground.py
+++ playground.py
@@ -2,6 +2,23 @@
 # First check out OpenAI / ChatGPT
 
 from dotenv import load_dotenv
+import os
 import openai
+import requests
 
+load_dotenv()
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+OPENAI_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+
+prompt = "Hi there, please tell me a joke"
+
+query = [{"role": "user", "content": prompt}]
+
+chat = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=query
+)
+
+print(chat)
